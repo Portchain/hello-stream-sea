ab7bf74f9025680173d2ea1958bfd9586ae731d2
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-02-26 10:27:14 +0100

    Add stream-sea client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as streamSea from 'stream-sea-client'
+
+const subscribeToStreamSea = async () => {
+  const subscription = await streamSea.subscribe({
+    stream: process.env.REACT_APP_STREAM_NAME!,
+    appId: process.env.REACT_APP_APP_ID!,
+    appSecret: process.env.REACT_APP_APP_SECRET!,
+    remoteServerHost: process.env.REACT_APP_REMOTE_SERVER_HOST!,
+    remoteServerPort: '443',
+    secure: true,
+  })
+  subscription.on('message', (msg:any) => {
+    console.log(JSON.stringify(msg))
+  })
+  subscription.on('error', (err:any) => console.error(err))
+  subscription.on('close', () =>console.log('Connection closed'))
+}
 
 function App() {
+  useEffect(() => {subscribeToStreamSea()}, [])
   return (
     <div className="App">
       <header className="App-header">
